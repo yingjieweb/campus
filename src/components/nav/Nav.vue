c361425db0fb9b03d8298471fb25318c3266b903
yingjieweb <1184061078@qq.com>
2020-09-06 21:39:36 +0800

    Nav component developing

---
+++ src/components/nav/Nav.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="nav">
+    <el-menu background-color="#20335D" text-color="#fff" style="border-right: none;border-bottom: 1px solid red;">
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-edit-outline"></i>
+          <span>考试系统</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="1-1">选项1</el-menu-item>
+          <el-menu-item index="1-2">选项2</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+      <el-menu-item index="2">
+        <i class="el-icon-document"></i>
+        <span slot="title">试题审核</span>
+      </el-menu-item>
+      <el-menu-item index="3">
+        <i class="el-icon-folder"></i>
+        <span slot="title">课程建设</span>
+      </el-menu-item>
+      <el-menu-item index="4">
+        <i class="el-icon-aim"></i>
+        <span slot="title">学校标准</span>
+      </el-menu-item>
+      <el-menu-item index="5">
+        <i class="el-icon-s-data"></i>
+        <span slot="title">数据中心</span>
+      </el-menu-item>
+      <el-menu-item index="6">
+        <i class="el-icon-cpu"></i>
+        <span slot="title">数据处理</span>
+      </el-menu-item>
+      <el-menu-item index="7">
+        <i class="el-icon-setting"></i>
+        <span slot="title">系统设置</span>
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Nav"
+  }
+</script>
+
+<style scoped>
+  .nav {
+    text-align: left;
+    width: 100%;
+    margin-right: -1px;
+  }
+</style>

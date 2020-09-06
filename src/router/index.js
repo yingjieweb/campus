d21d02f7c31f689e4cfd8508da0af5eaa578a56b
yingjieweb <1184061078@qq.com>
2020-09-06 16:35:10 +0800

    add Login component and router

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,23 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+
+import Login from "../views/Login"
 import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
+const routes = [
   {
     path: '/',
+    redirect: '/login'
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/home',
     name: 'Home',
     component: Home
   }
---
+++ src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="login">
+    登录
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: "Login"
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

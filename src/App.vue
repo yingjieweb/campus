f3705ed25df5eec9886f54d0156374baa81ad373
yingjieweb <1184061078@qq.com>
2020-09-08 22:32:15 +0800

    Fixed vuex's state loss during page refresh

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,20 @@
   </div>
 </template>
 
+<script>
+  export default {
+    name: 'App',
+    created(){
+      if (localStorage.getItem('store')){
+        this.$store.replaceState(Object.assign({}, this.$store.state, JSON.parse(localStorage.getItem("store"))));
+      }
+      window.addEventListener('beforeunload', () => {
+        localStorage.setItem('store', JSON.stringify(this.$store.state))
+      })
+    }
+  }
+</script>
+
 <style lang="scss">
   @import "~@/assets/styles/reset.scss";
   @import "~@/assets/styles/helper.scss";

aa1ef430ec86c96e1fd1293ab665376c4635ec60
yingjieweb <1184061078@qq.com>
2020-09-08 11:15:45 +0800

    basic implement UserManage function

--- src/views/system/userManage/UserManage.vue
+++ src/views/system/userManage/UserManage.vue
@@ -2,7 +2,7 @@
   <Layout>
     <Block title="用户管理">
       <div slot="button">
-        <el-button type="primary" size="small" plain icon="el-icon-circle-plus-outline">新增</el-button>
+        <el-button type="primary" size="small" plain icon="el-icon-circle-plus-outline" @click="addUser">新增</el-button>
         <el-button type="danger" size="small" plain icon="el-icon-circle-close">删除</el-button>
         <el-button type="primary" size="small" plain icon="el-icon-download">下载模板</el-button>
         <el-button type="primary" size="small" plain icon="el-icon-upload">导入用户</el-button>
@@ -10,21 +10,32 @@
       <div slot="operation">
         <Search placeholder="登录名/姓名"></Search>
       </div>
+      <div slot="content">
+        <el-table :data="tableData" border style="width: 100%">
+          <el-table-column prop="date" label="日期" width="180"></el-table-column>
+          <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+          <el-table-column prop="address" label="地址"></el-table-column>
+        </el-table>
+        <UserManageModal ref="UserManageModal"></UserManageModal>
+      </div>
     </Block>
   </Layout>
 </template>
 
 <script>
   import Search from "@/components/search/Search"
+  import UserManageModal from "@/views/system/userManage/UserManageModal"
 
   export default {
     name: "UserManage",
     components: {
-      Search
+      Search,
+      UserManageModal
     },
     data() {
       return {
-        /*tableData: [{
+        isShowDialog: false,
+        tableData: [{
           date: '2016-05-02',
           name: '王小虎',
           address: '上海市普陀区金沙江路 1518 弄'
@@ -40,7 +51,12 @@
           date: '2016-05-03',
           name: '王小虎',
           address: '上海市普陀区金沙江路 1516 弄'
-        }]*/
+        }]
+      }
+    },
+    methods: {
+      addUser(){
+        this.$refs.UserManageModal.setDialogVisible(true);
       }
     }
   }
---
+++ src/views/system/userManage/UserManageModal.vue
@@ -0,0 +1,71 @@
+<template>
+  <el-dialog
+          title="添加用户"
+          :visible.sync="dialogVisible"
+          width="30%"
+          :before-close="handleClose">
+    <el-form :model="form">
+      <el-form-item label="登录名" :label-width="formLabelWidth">
+        <el-input v-model="form.username" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="姓名" :label-width="formLabelWidth">
+        <el-input v-model="form.name" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="性别" :label-width="formLabelWidth">
+        <el-input v-model="form.gender" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="电话" :label-width="formLabelWidth">
+        <el-input v-model="form.phone" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="地址" :label-width="formLabelWidth">
+        <el-input v-model="form.address" autocomplete="off"></el-input>
+      </el-form-item>
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button size="small" @click="dialogVisible = false">取 消</el-button>
+      <el-button type="primary" size="small" @click="onsubmit">确 定</el-button>
+    </div>
+  </el-dialog>
+</template>
+
+<script lang="ts">
+  export default {
+    name: "UserManageModal",
+    data() {
+      return {
+        dialogVisible: false,
+        form: {
+          username: '',
+          name: '',
+          gender: '',
+          phone: '',
+          address: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: ''
+        },
+        formLabelWidth: '60px'
+      }
+    },
+    methods: {
+      setDialogVisible(state){
+        this.dialogVisible = state;
+      },
+      handleClose(done) {
+        // this.$confirm('确认关闭？').then(_ => {done();}).catch(_ => {});
+        done()
+      },
+      onsubmit(){
+        this.$message({message: '添加成功！', type: 'success'});
+        this.dialogVisible = false;
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+ >>> el-dialog__body {
+    padding: 0px;
+  }
+</style>

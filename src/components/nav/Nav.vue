8405131833fba1c433115fc3ddd45e5f96d0df51
yingjieweb <1184061078@qq.com>
2020-09-13 11:06:12 +0800

    fixed NavMenu click change color problem

--- src/components/nav/Nav.vue
+++ src/components/nav/Nav.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="nav">
-    <el-menu :default-active="$route.path" @select="handleSelect" background-color="#20335D" text-color="#fff" style="border-right: none;">
+    <el-menu
+      :default-active="$route.path"
+      @select="handleSelect"
+      background-color="#20335D"
+      text-color="#fff"
+      style="border-right: none;"
+      router>
       <el-submenu index="1">
         <template slot="title"><i class="el-icon-edit-outline"></i><span>考试系统</span></template>
         <el-menu-item-group>
@@ -12,12 +18,10 @@
         <i class="el-icon-document"></i>
         <span slot="title">试题审核</span>
       </el-menu-item>
-      <router-link to="/course-construct" style="font-size: inherit;">
-        <el-menu-item index="3">
-            <i class="el-icon-folder"></i>
-            <span slot="title">课程建设</span>
-        </el-menu-item>
-      </router-link>
+      <el-menu-item index="/course-construct">
+          <i class="el-icon-folder"></i>
+          <span slot="title">课程建设</span>
+      </el-menu-item>
       <el-submenu index="4">
         <template slot="title"><i class="el-icon-aim"></i><span>学校标准</span></template>
         <el-menu-item-group>
@@ -36,29 +40,23 @@
       <el-submenu index="7">
         <template slot="title"><i class="el-icon-setting"></i><span>系统设置</span></template>
         <el-menu-item-group>
-          <router-link to="/user-manage" style="font-size: inherit;">
-            <el-menu-item index="/user-manage">
-                <i class="el-icon-caret-right"></i>用户管理
-            </el-menu-item>
-          </router-link>
+          <el-menu-item index="/user-manage">
+              <i class="el-icon-caret-right"></i>用户管理
+          </el-menu-item>
           <el-menu-item index="7-2"><i class="el-icon-caret-right"></i>班级管理</el-menu-item>
           <el-menu-item index="7-3"><i class="el-icon-caret-right"></i>排课管理</el-menu-item>
           <el-menu-item index="7-4"><i class="el-icon-caret-right"></i>课程控制</el-menu-item>
           <el-menu-item index="7-5"><i class="el-icon-caret-right"></i>课程备份</el-menu-item>
         </el-menu-item-group>
       </el-submenu>
-      <router-link to="/formative-evaluation" style="font-size: inherit;">
-        <el-menu-item index="8">
-          <i class="el-icon-stopwatch"></i>
-          <span slot="title">形成性评价</span>
-        </el-menu-item>
-      </router-link>
-      <router-link to="/employ-recommend" style="font-size: inherit;">
-        <el-menu-item index="9">
-          <i class="el-icon-s-promotion"></i>
-          <span slot="title">应届生就业推荐</span>
-        </el-menu-item>
-      </router-link>
+      <el-menu-item index="/formative-evaluation">
+        <i class="el-icon-stopwatch"></i>
+        <span slot="title">形成性评价</span>
+      </el-menu-item>
+      <el-menu-item index="/employ-recommend">
+        <i class="el-icon-s-promotion"></i>
+        <span slot="title">应届生就业推荐</span>
+      </el-menu-item>
     </el-menu>
   </div>
 </template>

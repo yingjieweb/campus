959c25729e0c28c2dedf03cbda6cefb8b7aa135f
yingjieweb <1184061078@qq.com>
2020-09-13 11:18:34 +0800

    update CourseConstruct comp styles

--- src/views/courseConstruct/CourseConstruct.vue
+++ src/views/courseConstruct/CourseConstruct.vue
@@ -6,16 +6,24 @@
           <p>课程演示1</p>
           <p>演示方案</p>
           <p>教学大纲</p>
-          <p>课程信息</p>
+          <p style="background-color: #1296DB; padding: 2px; color: white;">课程信息</p>
         </div>
         <div class="detail">
-          <div class="buttons">编辑、结构导图、报告分析、达成度图谱</div>
+          <div class="buttons">
+            <el-button type="primary" size="small" plain >编辑</el-button>
+            <el-button type="primary" size="small" plain >结构导图</el-button>
+            <el-button type="primary" size="small" plain >报告分析</el-button>
+            <el-button type="primary" size="small" plain >达成度图谱</el-button>
+            <el-button type="primary" size="small" plain >持续改进</el-button>
+            <el-button type="primary" size="small" plain >支撑文件</el-button>
+            <el-button type="primary" size="small" plain >设置权重</el-button>
+          </div>
           <div class="classify">
             <div class="classify1">
               <p><i class="el-icon-pie-chart" ></i></p>
-              <p>完成度</p>
+              <p>完成度区</p>
             </div>
-            <div class="classify2">达成取</div>
+            <div class="classify2">达成度区</div>
             <div class="classify3">资源区</div>
           </div>
         </div>
@@ -33,19 +41,25 @@
 <style lang="scss" scoped>
   .container {
     display:flex;
+    min-height: 170px;
 
     > .scheme {
       flex: 2;
       text-align: center;
-      border: 1px solid #E6E8ED;
+      border: 2px solid #E6E8ED;
       border-radius: 4px;
       margin-right: 10px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
     }
 
     > .detail {
       flex: 10;
       border: 1px solid #E6E8ED;
       border-radius: 4px;
+
+      > .buttons { margin: 4px;}
 
       > .classify {
         height: 80px;

c11fecd2b47097d9cce493603810155433c67f20
yingjieweb <1184061078@qq.com>
2020-09-13 08:04:22 +0800

    update CourseConstruct styles

--- src/views/courseConstruct/CourseConstruct.vue
+++ src/views/courseConstruct/CourseConstruct.vue
@@ -1,19 +1,22 @@
 <template>
   <Layout>
     <Block title="课程建设">
-      <div slot="content" style="display:flex; border: 1px solid green;">
-        <div style="flex: 2; text-align: center; border: 1px solid red; margin-right: 10px;">
+      <div slot="content" class="container">
+        <div class="scheme">
           <p>课程演示1</p>
           <p>演示方案</p>
           <p>教学大纲</p>
           <p>课程信息</p>
         </div>
-        <div style="flex: 10; border: 1px solid red;">
-          <div style="border: 1px solid red;">编辑、结构导图、报告分析、达成度图谱</div>
-          <div style="display:flex; height: 80px;">
-            <div style="flex: 3; border: 1px solid red;">完成度</div>
-            <div style="flex: 3; border: 1px solid red;">达成取</div>
-            <div style="flex: 4; border: 1px solid red;">资源区</div>
+        <div class="detail">
+          <div class="buttons">编辑、结构导图、报告分析、达成度图谱</div>
+          <div class="classify">
+            <div class="classify1">
+              <p><i class="el-icon-pie-chart" ></i></p>
+              <p>完成度</p>
+            </div>
+            <div class="classify2">达成取</div>
+            <div class="classify3">资源区</div>
           </div>
         </div>
       </div>
@@ -28,5 +31,42 @@
 </script>
 
 <style lang="scss" scoped>
+  .container {
+    display:flex;
 
+    > .scheme {
+      flex: 2;
+      text-align: center;
+      border: 1px solid #E6E8ED;
+      border-radius: 4px;
+      margin-right: 10px;
+    }
+
+    > .detail {
+      flex: 10;
+      border: 1px solid #E6E8ED;
+      border-radius: 4px;
+
+      > .classify {
+        height: 80px;
+        display:flex;
+
+        > .classify1 {
+          flex: 3;
+          border: 1px solid #E6E8ED;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+
+          i {
+            font-size: 2em;
+            color: #1296DB;
+          }
+        }
+        > .classify2 {flex: 3; border: 1px solid #E6E8ED;}
+        > .classify3 {flex: 4; border: 1px solid #E6E8ED;}
+      }
+    }
+  }
 </style>

9ad663b72a82aba64dbec11702c3d89469f8bd55
yingjieweb <1184061078@qq.com>
2020-09-14 10:25:51 +0800

    FormativeEvaluation basic structure

--- src/views/formativeEvaluation/FormativeEvaluation.vue
+++ src/views/formativeEvaluation/FormativeEvaluation.vue
@@ -1,16 +1,69 @@
 <template>
   <Layout>
-    <Block title="课程建设">
-      <div slot="content" style="display:flex; border: 1px solid green;">
-        形成性评价
+    <Block title="形成性评价">
+      <div slot="content" style="display:flex; border: 1px solid green; justify-content: space-between;">
+        <div>学生基本信息</div>
+        <div style="width: 750px; height: 750px; border: 1px solid gainsboro; justify-content: flex-end;">
+          <div ref="radarChart" style="width: 100%; height: 100%;"></div>
+        </div>
       </div>
     </Block>
   </Layout>
 </template>
 
-<script lang="ts">
+<script lang="js">
+  import echarts from 'echarts'
+
   export default {
-    name: "FormativeEvaluation"
+    name: "FormativeEvaluation",
+    mounted(){
+      this.init();
+    },
+    methods: {
+      init(){
+        const Chart = echarts.init(this.$refs.radarChart);
+        Chart.setOption({
+          tooltip: {},
+          legend: {
+            data: ['平均能力分布', '个人能力分布图']
+          },
+          radar: {
+            // shape: 'circle',
+            name: {
+              textStyle: {
+                color: '#fff',
+                backgroundColor: '#999',
+                borderRadius: 20,
+                padding: [3, 5]
+              }
+            },
+            indicator: [
+              { name: '学习能力', max: 6500},
+              { name: '管理能力', max: 16000},
+              { name: '信息沟通能力', max: 30000},
+              { name: '运动能力', max: 38000},
+              { name: '研发能力', max: 52000},
+              { name: '工作能力', max: 25000}
+            ]
+          },
+          series: [{
+            name: '预算 vs 开销（Budget vs spending）',
+            type: 'radar',
+            // areaStyle: {normal: {}},
+            data: [
+              {
+                value: [4300, 10000, 28000, 35000, 50000, 19000],
+                name: '平均能力分布'
+              },
+              {
+                value: [5000, 14000, 28000, 31000, 42000, 21000],
+                name: '个人能力分布图'
+              }
+            ]
+          }]
+        })
+      }
+    }
   }
 </script>
 

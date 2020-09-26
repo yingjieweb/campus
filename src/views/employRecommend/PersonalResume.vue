72b91a411da4bc39f9be2ca531ac79e9f504b269
yingjieweb <1184061078@qq.com>
2020-09-26 20:58:35 +0800

    use el-tabs reset PersonalResume layout

--- src/views/employRecommend/PersonalResume.vue
+++ src/views/employRecommend/PersonalResume.vue
@@ -2,26 +2,22 @@
   <Layout>
     <Block title="应届生就业推荐">
       <div slot="content">
-        <div class="container">
-          <div class="resume">
-            11111
-          </div>
-          <div class="sideNav">
-            <ul>
-              <li>基本信息</li>
-              <li>求职意向</li>
-              <li>教育经历</li>
-              <li>实习经历</li>
-              <li>学生干部经历</li>
-              <li>项目经验</li>
-              <li>外语情况</li>
-              <li>奖惩经历</li>
-              <li>家庭情况</li>
-              <li>自我评价</li>
-              <li>其他情况</li>
-            </ul>
-          </div>
-        </div>
+        <el-tabs tab-position="left" style="height: 750px;">
+          <el-tab-pane label="基本信息">
+            <div class="basicInfo" style="border: 1px solid red; height: 500px;">
+              基本信息
+            </div>
+          </el-tab-pane>
+          <el-tab-pane label="求职意向">求职意向</el-tab-pane>
+          <el-tab-pane label="教育经历">教育经历</el-tab-pane>
+          <el-tab-pane label="实习经历">实习经历</el-tab-pane>
+          <el-tab-pane label="学生干部经历">学生干部经历</el-tab-pane>
+          <el-tab-pane label="项目经验">项目经验</el-tab-pane>
+          <el-tab-pane label="奖惩经历">奖惩经历</el-tab-pane>
+          <el-tab-pane label="家庭情况">家庭情况</el-tab-pane>
+          <el-tab-pane label="自我评价">自我评价</el-tab-pane>
+          <el-tab-pane label="其他情况">其他情况</el-tab-pane>
+        </el-tabs>
       </div>
     </Block>
   </Layout>
@@ -34,17 +30,5 @@
 </script>
 
 <style lang="scss" scoped>
-  .container {
-    display: flex;
 
-    > .resume {
-      flex: 9;
-      border: 1px solid red;
-    }
-
-    > .sideNav {
-      flex: 1;
-      border: 1px solid red;
-    }
-  }
 </style>

b4565b12b92a43428e3cea17b3fb1b6229e6b73d
yingjieweb <1184061078@qq.com>
2020-09-29 10:56:18 +0800

    implement EducationExperience basic function

--- src/components/recommend/personalResume/EducationExperience.vue
+++ src/components/recommend/personalResume/EducationExperience.vue
@@ -1,21 +1,82 @@
 <template>
   <div class="educationExp">
     <ResumeViewTitle title="教育经历"></ResumeViewTitle>
-    教育经历
+    <div class="eduItems">
+      <div class="eduItem" v-for="(item, index) in eduExp" :key="index">
+        <div class="eduItemKey">{{item[0]}}</div>
+        <div class="eduItemValue">{{item[1]}}</div>
+      </div>
+    </div>
+    <el-button @click="editEduExp">编辑</el-button>
+    <el-button @click="addEduExp">添加</el-button>
+
+    <EducationExperienceModal ref="EducationExperienceModal" @editEduExp="updateEduExp"></EducationExperienceModal>
   </div>
 </template>
 
 <script lang="ts">
   import ResumeViewTitle from "./ResumeViewTitle.vue";
+  import EducationExperienceModal from "./EducationExperienceModal.vue";
 
   export default {
     name: "EducationExperience",
     components: {
-      ResumeViewTitle
+      ResumeViewTitle,
+      EducationExperienceModal
+    },
+    data(){
+      return {
+        eduExp: {
+          school: ['学校','东北大学'],
+          major: ['专业','软件工程'],
+          period: ['起止时间','2018.09 ~ 2021.07'],
+          record: ['学历','硕士'],
+          scoreRank: ['成绩排名','50%'],
+          studyType: ['学历类型', '统招全日制']
+        }
+      }
+    },
+    methods: {
+      editEduExp(){
+        this.$refs.EducationExperienceModal.setDialogVisible(true, this.eduExp)
+      },
+      addEduExp(){
+
+      },
+      updateEduExp(form){
+        this.eduExp.school = ['学校',form.school];
+        this.eduExp.major = ['专业',form.major];
+        this.eduExp.period = ['起止时间',form.period];
+        this.eduExp.record = ['学历',form.record];
+        this.eduExp.scoreRank = ['成绩排名',form.scoreRank];
+        this.eduExp.studyType = ['学历类型',form.studyType];
+      }
     }
   }
 </script>
 
 <style lang="scss" scoped>
+  .educationExp {
+    height: 750px;
+    margin-right: 8px;
 
+    > .eduItems {
+      display: flex;
+      flex-wrap: wrap;
+
+      > .eduItem {
+        width: 50%;
+        margin-bottom: 16px;
+
+        > .eduItemKey {
+          color: gray;
+          margin-bottom: 8px;
+        }
+
+        > .eduItemValue {
+          margin-bottom: 4px;
+        }
+      }
+    }
+  }
 </style>
---
+++ src/components/recommend/personalResume/EducationExperienceModal.vue
@@ -0,0 +1,81 @@
+<template>
+  <el-dialog
+          title="教育经历"
+          :visible.sync="dialogVisible"
+          width="30%"
+          :before-close="handleClose">
+    <el-form :model="form" :rules="rules" ref="basicInfoForm">
+      <el-form-item label="学校" :label-width="formLabelWidth" prop="school">
+        <el-input v-model="form.school" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="专业" :label-width="formLabelWidth" prop="major">
+        <el-input v-model="form.major" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="起止日期" :label-width="formLabelWidth" prop="period">
+        <el-input v-model="form.period" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="学历" :label-width="formLabelWidth" prop="record">
+        <el-input v-model="form.record" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="成绩排名" :label-width="formLabelWidth" prop="scoreRank">
+        <el-input v-model="form.scoreRank" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="成绩排名" :label-width="formLabelWidth" prop="studyType">
+        <el-input v-model="form.studyType" autocomplete="off"></el-input>
+      </el-form-item>
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button size="small" @click="dialogVisible = false">取 消</el-button>
+      <el-button type="primary" size="small" @click="onsubmit">确 定</el-button>
+    </div>
+  </el-dialog>
+</template>
+
+<script lang="ts">
+  export default {
+    name: "EducationExperienceModal",
+    data(){
+      return {
+        dialogVisible: false,
+        form: {
+          school: '',
+          major: '',
+          period: '',
+          record: '',
+          scoreRank: '',
+          studyType: ''
+        },
+        formLabelWidth: '80px',
+        rules: {}
+      }
+    },
+    methods: {
+      setDialogVisible(state, oldBasicInfo){
+        this.dialogVisible = state;
+        this.form.school = oldBasicInfo.school[1];
+        this.form.major = oldBasicInfo.major[1];
+        this.form.period = oldBasicInfo.period[1];
+        this.form.record = oldBasicInfo.record[1];
+        this.form.scoreRank = oldBasicInfo.scoreRank[1];
+        this.form.studyType = oldBasicInfo.studyType[1];
+      },
+      handleClose(done) {
+        // this.$confirm('确认关闭？').then(_ => {done();}).catch(_ => {});
+        done()
+      },
+      onsubmit(){
+        this.$refs.basicInfoForm.validate((valid) => {
+          if (valid){
+            this.$emit('editEduExp', this.form);
+            this.$message({message: '教育经历更新成功！', type: 'success'});
+            this.dialogVisible = false;
+          }
+        })
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

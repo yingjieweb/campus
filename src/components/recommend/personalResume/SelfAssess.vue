8dbc3190a74eb102b9fcbfcd5155b7d1bf3b634a
yingjieweb <1184061078@qq.com>
2020-10-07 20:33:33 +0800

    finish SelfAssess comp function

--- src/components/recommend/personalResume/SelfAssess.vue
+++ src/components/recommend/personalResume/SelfAssess.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="selfAssess">
     <ResumeViewTitle title="自我评价"></ResumeViewTitle>
-    自我评价
+    <div class="assessText">
+      <pre>{{selfAssess}}</pre>
+    </div>
+    <el-button @click="editSelfAssess">编辑</el-button>
+    <el-button @click="goNextItem">下一项</el-button>
 
-    <SelfAssessModal ref="SelfAssessModal"></SelfAssessModal>
+    <SelfAssessModal ref="SelfAssessModal" @updateSelfAssess="updateSelfAssess"></SelfAssessModal>
   </div>
 </template>
 
@@ -16,10 +20,28 @@
     components: {
       ResumeViewTitle,
       SelfAssessModal
+    },
+    data(){
+      return {
+        selfAssess: '真不错，感觉自己真不错'
+      }
+    },
+    methods: {
+      editSelfAssess(){
+        this.$refs.SelfAssessModal.setDialogVisible('编辑自我评价', true, this.selfAssess);
+      },
+      updateSelfAssess(form){
+        this.selfAssess = form.selfAssess;
+      },
+      goNextItem(){
+        this.$emit('goNextItem', 'seventh');
+      }
     }
   }
 </script>
 
 <style lang="scss" scoped>
-
+  .assessText {
+    margin-bottom: 16px;
+  }
 </style>
--- src/components/recommend/personalResume/SelfAssessModal.vue
+++ src/components/recommend/personalResume/SelfAssessModal.vue
@@ -1,10 +1,65 @@
 <template>
-  <div></div>
+  <el-dialog
+          :title="title"
+          :visible.sync="dialogVisible"
+          width="30%"
+          :before-close="handleClose">
+    <el-form :model="form" :rules="rules" ref="selfAssessForm" :label-width="formLabelWidth">
+      <el-form-item label="自我评价" prop="selfAssess">
+        <el-input
+                type="textarea"
+                :autosize="{ minRows: 4, maxRows: 7}"
+                placeholder="请输入内容"
+                v-model="form.selfAssess">
+        </el-input>
+      </el-form-item>
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button size="small" @click="dialogVisible = false">取 消</el-button>
+      <el-button type="primary" size="small" @click="onsubmit">确 定</el-button>
+    </div>
+  </el-dialog>
 </template>
 
 <script lang="js">
   export default {
-    name: "SelfAssessModal"
+    name: "SelfAssessModal",
+    data() {
+      return {
+        title: '',
+        dialogVisible: false,
+        form: {
+          selfAssess: ''
+        },
+        formLabelWidth: '80px',
+        rules: {
+          selfAssess: [
+            { required: true, message: '请输入学生姓名', trigger: 'blur' },
+          ]
+        }
+      }
+    },
+    methods: {
+      setDialogVisible(title, state, oldSelfAssess){
+        this.title = title;
+        this.dialogVisible = state;
+
+        this.form.selfAssess = oldSelfAssess;
+      },
+      handleClose(done) {
+        // this.$confirm('确认关闭？').then(_ => {done();}).catch(_ => {});
+        done()
+      },
+      onsubmit(){
+        this.$refs.selfAssessForm.validate((valid) => {
+          if (valid){
+            this.$emit('updateSelfAssess', this.form);
+            this.$message({message: '自我评价更新成功！', type: 'success'});
+            this.dialogVisible = false;
+          }
+        })
+      }
+    }
   }
 </script>
 

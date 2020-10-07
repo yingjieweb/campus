63ce320b93600259c045f15782d8168401d5a3b6
yingjieweb <1184061078@qq.com>
2020-10-07 12:00:20 +0800

    finish ProjectExperience comp function

--- src/components/recommend/personalResume/ProjectExperience.vue
+++ src/components/recommend/personalResume/ProjectExperience.vue
@@ -2,20 +2,124 @@
   <div class="projectExp">
     <ResumeViewTitle title="项目经验"></ResumeViewTitle>
 
+    <div class="projectItems" v-for="(projectExpItem,index) in projectExp">
+      <div
+              :class="index === 'describe' ? 'growWidth' : 'projectItem'"
+              v-for="(item, index) in projectExpItem"
+              :key="index">
+        <div class="projectItemKey">{{item[0]}}</div>
+        <div class="projectItemValue"><pre>{{item[1]}}</pre></div>
+      </div>
+      <el-button @click="editProjectExp(index)">编辑</el-button>
+      <el-button @click="deleteProjectExp(index)">删除</el-button>
+      <el-button @click="addProjectExp" v-if="index === projectExp.length-1">添加</el-button>
+      <el-button @click="goNextItem" v-if="index === projectExp.length-1">下一项</el-button>
+    </div>
+    <el-button @click="addProjectExp" v-if="projectExp.length === 0">添加</el-button>
+    <el-button @click="goNextItem" v-if="projectExp.length === 0">下一项</el-button>
+
+    <ProjectExperienceModal ref="ProjectExperienceModal" @updateProjectExp="updateProjectExp"></ProjectExperienceModal>
   </div>
 </template>
 
 <script lang="js">
   import ResumeViewTitle from "@/components/recommend/personalResume/ResumeViewTitle"
+  import ProjectExperienceModal from "@/components/recommend/personalResume/ProjectExperienceModal"
 
   export default {
     name: "ProjectExperience",
     components: {
-      ResumeViewTitle
+      ResumeViewTitle,
+      ProjectExperienceModal
+    },
+    data(){
+      return {
+        projectExp: [{
+          projectName: ['项目名称','我的个人主页'],
+          projectRole: ['项目角色','前端模块负责人'],
+          period: ['起止时间','2018.09 ~ 2021.07'],
+          projectURL: ['项目链接','https://github.com/yingjieweb/resume'],
+          describe: ['项目描述', '一款基于 Vue+TypeScript 开发的个人主页，内容包含个人简介、博客、项目清单等']
+        }]
+      }
+    },
+    methods: {
+      addProjectExp(){
+        this.$refs.ProjectExperienceModal.setDialogVisible('新增项目经验', true, '', 'add');
+      },
+      editProjectExp(index){
+        this.$refs.ProjectExperienceModal.setDialogVisible('编辑项目经验', true, this.projectExp[index], 'edit', index);
+      },
+      deleteProjectExp(deleteIndex){
+        this.projectExp = this.projectExp.filter((item, index) => {
+          return index !== deleteIndex;
+        })
+      },
+      updateProjectExp(form, type, editItemIndex){
+        if (type === 'add'){
+          let newProjectExpItem = {};
+          newProjectExpItem.projectName = ['项目名称',form.projectName];
+          newProjectExpItem.projectRole = ['项目角色',form.projectRole];
+          newProjectExpItem.period = ['起止时间',form.period];
+          newProjectExpItem.projectURL = ['项目链接',form.projectURL];
+          newProjectExpItem.describe = ['项目描述',form.describe];
+          this.projectExp.push(newProjectExpItem);
+        }else {
+          this.projectExp[editItemIndex].projectName = ['项目名称',form.projectName];
+          this.projectExp[editItemIndex].projectRole = ['项目角色',form.projectRole];
+          this.projectExp[editItemIndex].period = ['起止时间',form.period];
+          this.projectExp[editItemIndex].projectURL = ['项目链接',form.projectURL];
+          this.projectExp[editItemIndex].describe = ['项目描述',form.describe];
+        }
+      },
+      goNextItem(){
+        this.$emit('goNextItem', 'fifth');
+      }
     }
   }
 </script>
 
 <style lang="scss" scoped>
+  .projectExp {
+    height: 750px;
+    margin-right: 8px;
+    overflow-y: auto;
 
+    > .projectItems {
+      display: flex;
+      flex-wrap: wrap;
+      margin-bottom: 12px;
+      &:hover {
+        background-color: #F9FBFF;
+      }
+
+      > .projectItem {
+        width: 50%;
+        margin-bottom: 16px;
+
+        > .projectItemKey {
+          color: gray;
+          margin-bottom: 8px;
+        }
+
+        > .projectItemValue {
+          margin-bottom: 4px;
+        }
+      }
+
+      > .growWidth {
+        width: 100%;
+        margin-bottom: 16px;
+
+        > .projectItemKey {
+          color: gray;
+          margin-bottom: 8px;
+        }
+
+        > .projectItemValue {
+          margin-bottom: 4px;
+        }
+      }
+    }
+  }
 </style>
--- src/components/recommend/personalResume/ProjectExperienceModal.vue
+++ src/components/recommend/personalResume/ProjectExperienceModal.vue
@@ -1,10 +1,122 @@
 <template>
-  <div></div>
+  <el-dialog
+          :title="title"
+          :visible.sync="dialogVisible"
+          width="30%"
+          :before-close="handleClose">
+    <el-form :model="form" :rules="rules" ref="internForm" :label-width="formLabelWidth">
+      <el-form-item label="项目名称" prop="projectName">
+        <el-input v-model="form.projectName" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="项目角色" prop="projectRole">
+        <el-input v-model="form.projectRole" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="起止日期" prop="period">
+        <el-date-picker
+                v-model="form.period"
+                type="daterange"
+                range-separator="至"
+                start-placeholder="开始日期"
+                end-placeholder="结束日期"
+                style="width: 100%;">
+        </el-date-picker>
+      </el-form-item>
+      <el-form-item label="项目链接" prop="projectURL">
+        <el-input v-model="form.projectURL" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="项目描述" prop="describe">
+        <el-input
+                type="textarea"
+                :autosize="{ minRows: 4, maxRows: 7}"
+                placeholder="请输入内容"
+                v-model="form.describe">
+        </el-input>
+      </el-form-item>
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button size="small" @click="dialogVisible = false">取 消</el-button>
+      <el-button type="primary" size="small" @click="onsubmit">确 定</el-button>
+    </div>
+  </el-dialog>
 </template>
 
 <script lang="js">
+  import {dateFormat} from "@/utils/dateFormat"
+
   export default {
-    name: "ProjectExperienceModal"
+    name: "ProjectExperienceModal",
+    data(){
+      return {
+        title: '',
+        type: '',
+        editItemIndex: '',
+        dialogVisible: false,
+        form: {
+          projectName: '',
+          projectRole: '',
+          period: '',
+          projectURL: '',
+          describe: ''
+        },
+        formLabelWidth: '80px',
+        rules: {
+          projectName: [
+            { required: true, message: '请输入项目名称', trigger: 'blur' }
+          ],
+          projectRole: [
+            { required: true, message: '请输入公司角色', trigger: 'blur' }
+          ],
+          period: [
+            { required: true, message: '请选择起止日期', trigger: 'blur' }
+          ],
+          projectURL: [
+            { required: true, message: '请输入项目链接', trigger: 'blur' }
+          ],
+          describe: [
+            { required: true, message: '请输入项目描述', trigger: 'blur' }
+          ]
+        }
+      }
+    },
+    methods: {
+      setDialogVisible(title, state, oldProjectExp, type, index) {
+        this.title = title;
+        this.dialogVisible = state;
+        this.type = type;
+
+        if (type === 'edit') {
+          this.editItemIndex = index;
+          this.form.projectName = oldProjectExp ? oldProjectExp.projectName[1] : '';
+          this.form.projectRole = oldProjectExp ? oldProjectExp.projectRole[1] : '';
+          this.form.period = oldProjectExp ? oldProjectExp.period[1] : '';
+          this.form.projectURL = oldProjectExp ? oldProjectExp.projectURL[1] : '';
+          this.form.describe = oldProjectExp ? oldProjectExp.describe[1] : '';
+        } else {
+          this.form.projectName = '';
+          this.form.projectRole = '';
+          this.form.period = '';
+          this.form.projectURL = '';
+          this.form.describe = '';
+        }
+      },
+      handleClose(done) {
+        // this.$confirm('确认关闭？').then(_ => {done();}).catch(_ => {});
+        done()
+      },
+      onsubmit() {
+        this.$refs.internForm.validate((valid) => {
+          if (valid) {
+            let preTime = dateFormat(this.form.period[0]);
+            let nextTime = dateFormat(this.form.period[1]);
+            this.form.period = `${preTime} ~ ${nextTime}`;
+
+            this.$emit('updateProjectExp', this.form, this.type, this.editItemIndex);
+            this.$message({message: '项目经验更新成功！', type: 'success'});
+            this.dialogVisible = false;
+          }
+        })
+      }
+    }
   }
 </script>
 

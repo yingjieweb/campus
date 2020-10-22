5894ebb56e0325ed8efc50e5e2bdf64bb3e25bf9
yingjieweb <1184061078@qq.com>
2020-10-22 09:50:54 +0800

    implement ScoreEnquiry's basic layout

--- src/views/scoreEnquiry/ScoreEnquiry.vue
+++ src/views/scoreEnquiry/ScoreEnquiry.vue
@@ -2,7 +2,10 @@
   <Layout>
     <Block title="学生成绩查询">
       <div slot="content">
-        学生成绩查询...
+        <el-table :data="tableData" stripe style="width: 100%">
+          <el-table-column prop="courseName" label="课程名" width="250"></el-table-column>
+          <el-table-column prop="courseScore" label="成绩"></el-table-column>
+        </el-table>
       </div>
     </Block>
   </Layout>
@@ -10,7 +13,20 @@
 
 <script lang="js">
   export default {
-    name: "ScoreEnquiry"
+    name: "ScoreEnquiry",
+    created() {
+      let computedTableData = [];
+      for (let item in this.scores){
+        computedTableData.push(Object.assign({}, {courseName: item, courseScore: this.scores[item]}))
+      }
+      this.tableData = computedTableData;
+    },
+    data() {
+      return {
+        scores: this.$store.state.currentUser.scores,
+        tableData: []
+      }
+    }
   }
 </script>
 

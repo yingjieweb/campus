8da034b0f5b577053102e2186fe07664bfdad1a1
yingjieweb <1184061078@qq.com>
2021-01-06 21:41:58 +0800

    optimize router tags styles

--- src/components/tags/Tags.vue
+++ src/components/tags/Tags.vue
@@ -2,8 +2,8 @@
   <div class="container" v-if="showTags">
     <ul class="tags">
       <li class="tagsItem" v-for="(item, index) in tagsList" :key="item.title" :class="{'active': isActive(item.path)}">
-        <router-link :to="item.path" class="tags-li-title">{{item.title}} </router-link>
-        <span class="tags-li-icon" @click="closeTags(index)"><i class="el-icon-close"></i></span>
+        <router-link :to="item.path" class="tags-li-title">{{item.title}}</router-link>
+        <i class="el-icon-close" @click="closeTags(index)"></i>
       </li>
     </ul>
   </div>
@@ -19,7 +19,7 @@
     },
     methods: {
       isActive(path) {
-        return path === this.$route.fullPath;
+        return path === this.$route.fullPath
       },
       setTags(route) {
         if (!route.name) return // 清除重定向的路由信息
@@ -37,11 +37,11 @@
         }
       },
       closeTags(index) {
-        const delItem = this.tagsList.splice(index, 1)[0];
-        const item = this.tagsList[index] ? this.tagsList[index] : this.tagsList[index - 1];
+        const delItem = this.tagsList.splice(index, 1)[0]
+        const item = this.tagsList[index] ? this.tagsList[index] : this.tagsList[index - 1]
         if (item) {
-          delItem.path === this.$route.fullPath && this.$router.push(item.path);
-        }else{
+          delItem.path === this.$route.fullPath && this.$router.push(item.path)
+        } else {
           this.tagsList = [{
             path: "/work-bench",
             title: "系统首页"
@@ -72,11 +72,10 @@
 
     .tagsItem {
       color: dimgrey;
-      padding: 2px 4px;
+      padding: 4px 5px;
       margin: 0 2px;
       font-size: 16px;
       line-height: 16px;
-      white-space: nowrap;
       border: 1px solid #d3d4d6;
       border-radius: 4px;
       background-color: #fff;

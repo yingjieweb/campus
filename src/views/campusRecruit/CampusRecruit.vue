baca423c86224d227d6571ed53d0ec175b6bf3d9
yingjieweb <1184061078@qq.com>
2021-01-08 22:45:01 +0800

    CampusRecruit basic layout

--- src/views/campusRecruit/CampusRecruit.vue
+++ src/views/campusRecruit/CampusRecruit.vue
@@ -1,8 +1,13 @@
 <template>
-  <Block title="校园招聘">
+  <Block title="校园招聘" style="height: 1200px;">
     <div slot="content" class="container">
-      <input type="text" placeholder="搜索职位、公司、地点">
-      <button>搜索</button>
+      <div class="search">
+        <input type="text" placeholder="搜索职位、公司、地点">
+        <button>搜索</button>
+      </div>
+      <div class="banner"></div>
+      <div class="hot-company"></div>
+      <div class="hot-job"></div>
     </div>
   </Block>
 </template>
@@ -15,8 +20,25 @@
 
 <style lang="scss" scoped>
   .container {
-    border: 1px solid red;
-    display: flex;
-    justify-content: center;
+
+    .search {
+      height: 50px;
+      border: 1px solid red;
+    }
+
+    .banner {
+      height: 200px;
+      border: 1px solid red;
+    }
+
+    .hot-company {
+      height: 200px;
+      border: 1px solid red;
+    }
+
+    .hot-job {
+      height: 200px;
+      border: 1px solid red;
+    }
   }
 </style>

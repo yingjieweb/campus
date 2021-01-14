cc4b9ce2c3a6c60c14fdc8d156ef43caeb7e5ed1
yingjieweb <1184061078@qq.com>
2021-01-14 22:18:06 +0800

    set HotCompany basic layout

--- src/views/hotCompany/HotCompany.vue
+++ src/views/hotCompany/HotCompany.vue
@@ -1,7 +1,14 @@
 <template>
   <Block title="热门公司" style="height: 1200px;">
     <div slot="content" class="container">
-      热门公司
+      <div class="worth-recommend">
+        什么值的投
+      </div>
+      <div class="company-list">
+        <div class="company-item">
+          美团
+        </div>
+      </div>
     </div>
   </Block>
 </template>
@@ -13,5 +20,13 @@
 </script>
 
 <style lang="scss" scoped>
+  .container {
+    .worth-recommend {
+      border: 1px solid red;
+    }
 
+    .company-list {
+      border: 1px solid red;
+    }
+  }
 </style>

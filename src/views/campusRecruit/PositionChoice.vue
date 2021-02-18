910288425927f523d30abd2d9fd7956b36034aaf
yingjieweb <1184061078@qq.com>
2021-02-18 20:54:11 +0800

    PositionChoice hover styles

--- src/views/campusRecruit/PositionChoice.vue
+++ src/views/campusRecruit/PositionChoice.vue
@@ -1,15 +1,12 @@
 <template>
   <div class="container">
-    <div class="position-type"
-         v-for="item in 7"
-         @mouseenter="focusPositionChoice"
-         @mouseleave="unfocusPositionChoice">
+    <div class="position-sample" v-for="item in 7">
       <span class="title">技术</span>
-      <span class="type-behalf">Java</span>
-      <span class="type-behalf">PHP</span>
-      <span class="type-behalf">区块链</span>
+      <span class="position-behalf">Java</span>
+      <span class="position-behalf">PHP</span>
+      <span class="position-behalf">区块链</span>
       <span class="arrow">></span>
-      <div class="type-items" :class="{'visible': typeItemsVisible}">
+      <div class="position-details">
         这就是一些items
       </div>
     </div>
@@ -18,48 +15,47 @@
 
 <script lang="js">
   export default {
-    name: "PositionChoice",
-    data() {
-      return {
-        typeItemsVisible: false
-      }
-    },
-    methods: {
-      focusPositionChoice() {
-        this.focusPositionChoice = true
-      },
-      unfocusPositionChoice() {
-        this.focusPositionChoice = false
-      }
-    }
+    name: "PositionChoice"
   }
 </script>
 
 <style lang="scss" scoped>
   .container {
-    .position-type {
+    .position-sample {
       height: 50px;
       font-size: 18px;
-      line-height: 50px;
+      line-height: 18px;
       font-weight: 500;
       padding-left: 15px;
       position: relative;
       display: flex;
       align-items: center;
       justify-content: space-around;
-      border: 1px solid red;
 
-      .type-behalf, .arrow {
+      .position-behalf {
         cursor: pointer;
+
+        &:hover {
+          color: #55CBCA;
+          border-bottom: 1px solid #55CBCA;
+        }
       }
 
-      .type-items {
+      .position-details {
         position: absolute;
+        top: 0;
+        left: 100%;
+        width: 100%;
+        border: 1px solid red;
         display: none;
+        background-color: #fff;
       }
 
-      .visible {
-        display: block;
+      &:hover {
+        box-shadow: 0 0 4px 1px rgba(0 0 0 / 9%);
+        .position-details {
+          display: block;
+        }
       }
     }
   }

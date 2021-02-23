fc4df3a3a1f95f8aa31b846dab91c520ada893cb
yingjieweb <1184061078@qq.com>
2021-02-23 21:40:47 +0800

    optimize UserManage comp func

--- src/views/systemSetting/userManage/UserManage.vue
+++ src/views/systemSetting/userManage/UserManage.vue
@@ -7,7 +7,7 @@
       <el-button type="primary" size="small" plain icon="el-icon-upload">导入用户</el-button>
     </div>
     <div slot="operation">
-      <Search placeholder="登录名/姓名"></Search>
+      <Search placeholder="请输入搜索信息"></Search>
     </div>
     <div slot="content">
       <el-table
@@ -17,11 +17,18 @@
               border
               @selection-change="selsChange">
         <el-table-column type="selection" width="55"></el-table-column>
-        <el-table-column prop="username" label="登录名"></el-table-column>
         <el-table-column prop="name" label="姓名"></el-table-column>
+        <el-table-column prop="grade" label="年级"></el-table-column>
         <el-table-column prop="gender" label="性别"></el-table-column>
+        <el-table-column prop="studentId" label="学号"></el-table-column>
         <el-table-column prop="phone" label="电话"></el-table-column>
-        <el-table-column prop="address" label="地址" show-overflow-tooltip></el-table-column>
+        <el-table-column prop="address" label="家庭地址" show-overflow-tooltip></el-table-column>
+        <el-table-column fixed="right" label="操作" width="100">
+          <template slot-scope="scope">
+            <el-button type="text" size="small">编辑</el-button>
+            <el-button type="text" size="small">删除</el-button>
+          </template>
+        </el-table-column>
       </el-table>
       <UserManageModal ref="UserManageModal" @addUserInfo="addUserInfo"></UserManageModal>
     </div>
@@ -41,10 +48,11 @@
     data() {
       return {
         isShowDialog: false,
-        selectedUser: [],
+        selectedStudentId: [],
         tableData: [{
-          username: '一只小皮皮',
-          name: '王小虎',
+          name: '张磊',
+          grade: '2014级',
+          studentId: '2014416659',
           gender: '男',
           phone: '18860628958',
           address: '上海市普陀区金沙江路 1518 弄'
@@ -59,26 +67,25 @@
         this.tableData.push(newUserInfo);
       },
       selsChange(sels) {
-        this.selectedUser = sels.map(item => {
-          return item.id
+        this.selectedStudentId = sels.map(item => {
+          return item.studentId
         })
       },
       batchDeleteClick(){
-        if (this.selectedUser.length > 0) {
+        if (this.selectedStudentId.length > 0) {
           this.$confirm('是否确认删除当前已选用户信息？', '提示', {
             confirmButtonText: '确定',
             cancelButtonText: '取消',
             type: 'warning'
           }).then(() => {
-            console.log(this.selectedUser)
+            this.tableData = this.tableData.filter(item => {
+              return !this.selectedStudentId.includes(item.studentId)
+            })
           }).catch(() => {
             this.$message({message: '取消删除操作！', type: 'info'})
           })
         } else {
-          this.$message({
-            type: "info",
-            message: "请选择需要删除的用户信息！"
-          });
+          this.$message.info('请选择需要删除的用户信息！');
         }
       },
     }
--- src/views/systemSetting/userManage/UserManageModal.vue
+++ src/views/systemSetting/userManage/UserManageModal.vue
@@ -4,20 +4,23 @@
           :visible.sync="dialogVisible"
           width="30%"
           :before-close="handleClose">
-    <el-form :model="form">
-      <el-form-item label="登录名" :label-width="formLabelWidth">
-        <el-input v-model="form.username" autocomplete="off"></el-input>
-      </el-form-item>
-      <el-form-item label="姓名" :label-width="formLabelWidth">
+    <el-form :model="form" :rules="rules" ref="userModalForm">
+      <el-form-item label="姓名" prop="name" :label-width="formLabelWidth">
         <el-input v-model="form.name" autocomplete="off"></el-input>
       </el-form-item>
-      <el-form-item label="性别" :label-width="formLabelWidth">
+      <el-form-item label="年级" prop="grade" :label-width="formLabelWidth">
+        <el-input v-model="form.grade" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="学号" prop="studentId" :label-width="formLabelWidth">
+        <el-input v-model="form.studentId" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="性别" prop="gender" :label-width="formLabelWidth">
         <el-input v-model="form.gender" autocomplete="off"></el-input>
       </el-form-item>
-      <el-form-item label="电话" :label-width="formLabelWidth">
+      <el-form-item label="电话" prop="phone" :label-width="formLabelWidth">
         <el-input v-model="form.phone" autocomplete="off"></el-input>
       </el-form-item>
-      <el-form-item label="地址" :label-width="formLabelWidth">
+      <el-form-item label="家庭地址" prop="address" :label-width="formLabelWidth">
         <el-input v-model="form.address" autocomplete="off"></el-input>
       </el-form-item>
     </el-form>
@@ -34,35 +37,63 @@
     data() {
       return {
         dialogVisible: false,
+        formLabelWidth: '90px',
         form: {
-          username: '',
           name: '',
+          grade: '',
+          studentId: '',
           gender: '',
           phone: '',
           address: ''
         },
-        formLabelWidth: '60px'
+        rules: {
+          name: [{required: true, message: '请输入学生姓名', trigger: 'blur'}],
+          grade: [{required: true, message: '请输入学生年级', trigger: 'blur'}],
+          studentId: [{required: true, message: '请输入学生学号', trigger: 'blur'}],
+          gender: [{required: true, message: '请输入学生年龄', trigger: 'blur'}],
+          phone: [{required: true, message: '请输入学生电话', trigger: 'blur'}],
+          address: [{required: true, message: '请输入学生家庭地址', trigger: 'blur'}]
+        }
       }
     },
     methods: {
-      setDialogVisible(state){
+      setDialogVisible(state) {
         this.dialogVisible = state;
       },
       handleClose(done) {
         // this.$confirm('确认关闭？').then(_ => {done();}).catch(_ => {});
         done()
       },
-      onsubmit(){
-        this.$emit('addUserInfo', this.form);
-        this.$message({message: '添加成功！', type: 'success'});
-        this.dialogVisible = false;
+      onsubmit() {
+        this.$refs.userModalForm.validate((valid) => {
+          if (valid) {
+            this.$emit('addUserInfo', JSON.parse(JSON.stringify(this.form)));
+            this.$message({message: '添加成功！', type: 'success'});
+            this.dialogVisible = false;
+
+            this.$refs.userModalForm.resetFields();
+          } else {
+            this.$message.error('请按提示输入完整的学生信息~')
+          }
+        });
       }
     }
   }
 </script>
 
 <style lang="scss" scoped>
- >>> el-dialog__body {
-    padding: 0px;
-  }
+ ::v-deep .el-dialog {
+   .el-dialog__header {
+     border-bottom: 1px solid #DCDFE6;
+   }
+
+   .el-dialog__body {
+     padding: 20px 40px 10px;
+   }
+
+   .el-dialog__footer {
+     border-top: 1px solid #DCDFE6;
+   }
+ }
+
 </style>

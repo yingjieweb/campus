2bd27e3cc16c067299d5b6b788f79e9635a98f15
yingjieweb <1184061078@qq.com>
2021-02-23 22:23:33 +0800

    PositionSearch basic layout

--- src/views/positionSearch/PositionSearch.vue
+++ src/views/positionSearch/PositionSearch.vue
@@ -1,7 +1,14 @@
 <template>
   <Block title="职位搜索">
     <div slot="content">
-      职位搜索职位搜索职位搜索职位搜索职位搜索职位搜索
+      <div class="search">
+        <input type="text" placeholder="搜索职位、公司、地点">
+        <button>搜索</button>
+      </div>
+
+      <div class="condition">查询条件</div>
+
+      <div class="results">查询结果</div>
     </div>
   </Block>
 </template>
@@ -14,5 +21,33 @@
 </script>
 
 <style lang="scss" scoped>
+  .search {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 6px;
 
+    input {
+      width: 770px;
+      height: 42px;
+      line-height: 46px;
+      padding: 12px 8px 12px 16px;
+      font-size: 16px;
+      border: 1px solid #e8e8e8;
+      border-right: 0;
+      vertical-align: top;
+      outline: none;
+      color: #333;
+    }
+
+    button {
+      width: 140px;
+      height: 42px;
+      line-height: 46px;
+      font-size: 18px;
+      border: 1px solid #00b38a;
+      background: #00b38a;
+      color: #fff;
+      cursor: pointer;
+    }
+  }
 </style>
